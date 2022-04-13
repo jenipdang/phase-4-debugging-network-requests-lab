17ae1f477c0a3767936619fc5e149842902c729d
Jeni Dang <jenipdang@yahoo.com>
2022-04-12 17:48:14 -0700

    commit tree 618b272f196739e00bf4db9a818b94eb44731d15

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,4 +1,6 @@
+
 class ToysController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :not_found
   wrap_parameters format: []
 
   def index
@@ -7,25 +9,34 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    toy = find_toy
+    toy&.update!(likes: toy.likes + 1)
+    render json: toy_params, status: :ok
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.destroy
     head :no_content
   end
 
   private
   
+  def find_toy
+    Toy.find(params[:id])
+  end
+
   def toy_params
     params.permit(:name, :image, :likes)
+  end
+
+  def not_found
+    render json: { error: "Toy not found" }, status: :not_found
   end
 
 end
